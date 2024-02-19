634223e244d9996aec095b40f6a4b93c9aba7d65
spiralsky <shianastrophysics@gmail.com>
2024-02-19 22:48:40 +0800

    1.0.1 Added recipe Added tooltip

--- src/main/java/com/spiralsky/heavensaddons/Classes/Item/HolyGrailItem.java
+++ src/main/java/com/spiralsky/heavensaddons/Classes/Item/HolyGrailItem.java
@@ -1,13 +1,18 @@
 package com.spiralsky.heavensaddons.Classes.Item;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class HolyGrailItem extends Item {
     public HolyGrailItem(Properties properties) {
@@ -18,16 +23,20 @@ public class HolyGrailItem extends Item {
         ItemStack mainhandItem = player.getItemInHand(hand);
         ItemStack offhandItem = player.getOffhandItem();
 
-
         if (!world.isClientSide) {
             if (offhandItem.getTag() == null) {
                 offhandItem.setTag(new CompoundTag());
             }
-
             offhandItem.getTag().putBoolean("Unbreakable", true);
         }
 
         return new InteractionResultHolder<>(InteractionResult.SUCCESS, mainhandItem);
 
+    }
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level worldIn, List<Component> tooltip, TooltipFlag flagIn) {
+        tooltip.add(Component.literal("Right click to make off-hand item Unbreakable"));
+
+        super.appendHoverText(stack, worldIn, tooltip, flagIn);
     }
 }
